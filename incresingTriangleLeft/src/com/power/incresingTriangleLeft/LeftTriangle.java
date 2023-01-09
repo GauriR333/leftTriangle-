f3a20d44c1e85000d3aec3f65bd42315dd80dada
Gauri Ragade <gauriragade98@gmail.com>
2023-01-09 17:27:48 +0530

    Left Triangle with &

---
+++ incresingTriangleLeft/src/com/power/incresingTriangleLeft/LeftTriangle.java
@@ -0,0 +1,13 @@
+package com.power.incresingTriangleLeft;
+
+public class LeftTriangle {
+	public static void main(String args[]) {
+ 		int i, j, n = 5;
+		for (i = 1; i <= n; i++) {
+ 			for (j = 1; j <= i; j++) {
+				System.out.print("& ");
+			}
+			System.out.println();
+		}
+	}
+}
